298a3aa9eb796511c04d7d938143ec2c7b6154de
PunNapat240 <punpun.naput@gmail.com>
2024-10-15 19:00:59 +0700

    update

--- ticket-booking/src/components/MovieBox.jsx
+++ ticket-booking/src/components/MovieBox.jsx
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 
-function ConcertBox({ image, name, date, time, location, onBuyTickets }) {
+function MovieBox({ image, name, date, time, location, onBuyTickets }) {
   return (
-    <div className="mt-3 ml-2 p-2 bg-zinc-900 rounded-3xl max-w-lg">
-      <div className="flex">
-        <img src={image} alt={name} className="w-[40%] rounded-3xl" />
-        <div className="ml-3 flex flex-col ">
+    <div className="mt-3 ml-2 p-2 bg-zinc-900 rounded-3x1 max-w-lg">
+      <div className="flex-col">
+        <img src={image} alt={name} className="h-[400px] object-cover rounded-3xl" />
+        <div className="ml-3 flex flex-col text-center items-center">
           <div>
             <h2 className="text-lg font-extrabold text-white">{name}</h2>
             <p className="text-sm text-yellow-500 mt-3">
@@ -33,7 +33,7 @@ function ConcertBox({ image, name, date, time, location, onBuyTickets }) {
   );
 }
 
-ConcertBox.propTypes = {
+MovieBox.propTypes = {
   image: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
@@ -42,4 +42,4 @@ ConcertBox.propTypes = {
   onBuyTickets: PropTypes.func.isRequired,
 };
 
-export default ConcertBox;
+export default MovieBox;
--- ticket-booking/src/pages/Home.jsx
+++ ticket-booking/src/pages/Home.jsx
@@ -1,16 +1,23 @@
 import Navbar from "../components/navbar";
 import RecommendMovie from "../components/recommendMovie";
-import ConcertBox from "../components/ConcertBox";
+import MovieBox from "../components/MovieBox";
 import ShowTheaterAndMovie from "../components/showTheaterAndMovie";
+import moviesDetail from "../components/moviesDetail"; 
 
-import bp from "../assets/bp.png";
-import rv from "../assets/rv.png";
-import tw from "../assets/twice.png";
+// import bp from "../assets/moviePoster/Avengers.jpeg";
+// import rv from "../assets/moviePoster/in-side-out-2.jpeg";
+// import tw from "../assets/moviePoster/The-last-of-us.jpg";
+// import gf from "../assets/moviePoster/The-Garfield-Movie(2024).jpg";
 
 function Home() {
   const handleBuyTickets = () => {
     alert("Tickets purchase functionality is not implemented yet.");
   };
+
+  // const n1 ="Avengers"
+  // const n2 ="In Side Out 2"
+  // const n3 ="The Last of Us"
+  // const n4 ="The Garfield Movie"
 
   return (
     <div className="bg-black min-h-screen">
@@ -19,203 +26,23 @@ function Home() {
       <ShowTheaterAndMovie />
 
       <div className="pt-1 w-[60%] mx-auto">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
-          <ConcertBox
-            image={bp}
-            name="Black Pink"
-            date="2024-09-15"
-            time="19:00"
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3">
+          {moviesDetail.map((movie, key)=>(
+            <MovieBox
+            key={key}
+            image={movie.poster}
+            name={movie.title}
+            date={movie.releaseDate}
+            time={movie.duration}
             location="Concert Hall, City Center"
             onBuyTickets={handleBuyTickets}
           />
-          <ConcertBox
-            image={rv}
-            name="Red Velvet"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={tw}
-            name="Twice"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={bp}
-            name="Black Pink"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={rv}
-            name="Red Velvet"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={tw}
-            name="Twice"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={bp}
-            name="Black Pink"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={rv}
-            name="Red Velvet"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={tw}
-            name="Twice"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={bp}
-            name="Black Pink"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={rv}
-            name="Red Velvet"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={tw}
-            name="Twice"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={bp}
-            name="Black Pink"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={rv}
-            name="Red Velvet"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={tw}
-            name="Twice"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={bp}
-            name="Black Pink"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={rv}
-            name="Red Velvet"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={tw}
-            name="Twice"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={bp}
-            name="Black Pink"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={rv}
-            name="Red Velvet"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={tw}
-            name="Twice"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={bp}
-            name="Black Pink"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={rv}
-            name="Red Velvet"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
-          <ConcertBox
-            image={tw}
-            name="Twice"
-            date="2024-09-15"
-            time="19:00"
-            location="Concert Hall, City Center"
-            onBuyTickets={handleBuyTickets}
-          />
+          ))}
+          
         </div>
       </div>
     </div>
-  );
-}
+  );  
+};
 
 export default Home;
